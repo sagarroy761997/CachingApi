051d557bf2b97e80e052df60a6666eb84d17989f
sagarroy761997 <sagar.r@celestialsys.com>
2025-02-09 13:39:41 +0530

    added details in root

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const {marked} = require('marked');
 const app = express();
 const port = process.env.PORT || 8888;
 
